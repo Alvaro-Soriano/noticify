359d4ff4024542274fe598ad3c02eb0ba8e4a9bd
Alvaro-Soriano <alvarosoriano2812@gmail.com>
2026-02-10 18:34:55 +0100

    created free search

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express= require('express');
 const app = express();
+const path = require('path');
 const port = 2000;
 
+app.use('/css', express.static('frontend/css'));
+app.use('/js', express.static('frontend/js'));
 
 
+app.get('/',(req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "html", 'index.html'));
+});
+app.get('/freesearch',(req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "html", 'freesearch.html'));
+});
 
 app.listen(port);
