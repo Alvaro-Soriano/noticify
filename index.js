3c41af3b27e56f24c1ac3a678751141fdbd98d18
Alvaro-Soriano <alvarosoriano2812@gmail.com>
2026-02-21 17:14:03 +0100

    subidas imagenes, repensada del proyecto

--- index.js
+++ index.js
@@ -2,25 +2,20 @@ const express= require('express');
 const app = express();
 const path = require('path');
 const port = 2000;
+const cors = require('cors');
 
+app.use(cors());
 app.use('/css', express.static('frontend/css'));
 app.use('/js', express.static('frontend/js'));
+app.use('/flags', express.static('frontend/images/flags'));
+
+app.get('/getcountries', (req, res)=>{
+	res.sendFile(path.join(__dirname, "static", 'countries.json'));
+});
 
 
 app.get('/',(req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "html", 'index.html'));
-});
-app.get('/freesearch',(req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "html", 'freesearch.html'));
-});
-app.get('/map',(req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "html", 'map.html'));
-});
-app.get('/stats',(req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "html", 'stats.html'));
-});
-app.get('/config',(req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "html", 'config.html'));
 });
 
 app.listen(port);
