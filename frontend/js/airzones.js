45be335d95c84b0b7367c6ab2cc70d0bf981ab93
Alvaro-Soriano <alvarosoriano2812@gmail.com>
2026-03-06 18:08:34 +0100

    pruebas de diseño y modif de airzones.json para incluir datos algo mejores y quitar palabras redundates de los labels

--- frontend/js/airzones.js
+++ frontend/js/airzones.js
@@ -4,6 +4,8 @@ const countryCont = document.getElementById('countryCont');
 
 fetch(airzonesE).then(d => d.json()).then((d)=>{
 	const countries = [...new Set(d.map((icao)=>icao.country))];
+	const types = [...new Set(d.map((icao)=>icao.type))];
+	console.log(types);
 	for(let con of countries){
 		const flagBlock = document.createElement('div');
 		flagBlock.classList.add('flagBlock');
